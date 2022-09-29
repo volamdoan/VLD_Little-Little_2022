9020fe18c434f3b6578a6d315782f21ba7929d68
volamdoan <95078093+volamdoan@users.noreply.github.com>
2022-09-29 15:18:34 +0700

    Update Code
    Update Code

--- Little-little/routes/web.php
+++ Little-little/routes/web.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/',[App\Http\Controllers\HomeController::class, 'index']);
 
 
-Route::get('/su-kien',[App\Http\Controllers\EventController::class, 'index'])->name('event');
-Route::get('/show/{id}',[App\Http\Controllers\EventController::class, 'show'])->name('event.show');
+Route::get('/su-kien',[App\Http\Controllers\EventController::class, 'index']);
+Route::get('/show/{id}',[App\Http\Controllers\EventController::class, 'show']);
 
 
-Route::get('/lien-he',[App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
-Route::post('/send-mail',[App\Http\Controllers\HomeController::class, 'sendMail'])->name('sendmail');
+Route::get('/lien-he',[App\Http\Controllers\HomeController::class, 'contact']);
+Route::post('/send-mail',[App\Http\Controllers\HomeController::class, 'sendMail']);
 
 // Route::get('/thanh-toan',function(){
 //     return view('checkout.checkout');
 // });
 
-Route::post('/thanh-toan',[App\Http\Controllers\CheckoutController::class, 'checkout'])->name('checkout');
+Route::post('/thanh-toan',[App\Http\Controllers\CheckoutController::class, 'checkout']);
